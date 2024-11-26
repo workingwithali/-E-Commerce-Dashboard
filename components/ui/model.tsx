212e84a4bf67fa67398a06e1ed3fb552e56879ca
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-26 17:10:57 +0500

    update

--- components/ui/model.tsx
+++ components/ui/model.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Dialog } from "./dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "./dialog";
 
 
 
@@ -24,8 +24,17 @@ export const Modal: React.Fc<ModalProps>=({
         }
     };
     return (
-        <Dialog>
-            
+        <Dialog open={isOpen} onOpenChange={onChange} >
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>{description}</DialogDescription>
+                </DialogHeader>
+                <div>
+                    {children}
+                </div>
+            </DialogContent>
+
         </Dialog>
     )
 }
