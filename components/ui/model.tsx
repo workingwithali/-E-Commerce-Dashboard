aa729bc82c8ed3da4c9e57bb61063ed17210f284
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-26 17:05:28 +0500

    update

--- components/ui/model.tsx
+++ components/ui/model.tsx
@@ -1,6 +1,24 @@
 "use client"
+
+
+
 interefacec ModalProps{
     title:string;
-    discrption:string;
-    isOpen:b
+    description:string;
+    isOpen:boolean;
+    onClose:()=> void;
+    children?: React.ReactNode;
 };
+export const Modal: React.Fc<ModalProps>=({
+    title,
+    description,
+    isOpen,
+    onClose,
+    children
+})=>{
+    const onChange = (open:boolean)=>{
+        if(!open){
+            onClose();
+        }
+    }
+}
