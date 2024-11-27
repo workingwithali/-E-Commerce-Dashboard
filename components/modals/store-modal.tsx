4454685fb76971f4555f51cca7ed8bc50e62aca0
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-27 15:44:52 +0500

    update

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -2,10 +2,20 @@
 
 import { useStoreModal } from '@/hooks/use-state-model'
 import { Modal } from '@/components/ui/modal'; 
+import * as z from 'zod'
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
+const formSchema  = z.object({
+    name: z.string().min(1),
+});
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
+    
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver : zodResolver(formSchema)
+    })
 
     return (
         <Modal
