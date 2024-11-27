316c7f339298c92b33d8bb01d92221ca37e230de
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-27 08:10:21 +0500

    update

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -11,7 +11,7 @@ export const ModalProvider = () => {
     }, []); 
 
     if (!isMount) {
-        return null; // Render nothing until the component is mounted
+        return null; 
     }
 
     return (
